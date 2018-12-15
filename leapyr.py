b92ebb65fb68e863e1b881b5010957aa3d0a9667
santhoshnec <38747301+santhoshnec@users.noreply.github.com>
2018-12-15 16:01:02 +0530

    Create leapyr.py

---
+++ leapyr.py
@@ -0,0 +1,11 @@
+a=int(input())
+if(a%4==0):
+    if(a%100==0):
+        if(a%400==0):
+            print("leap year")
+        else:
+            print("not leap year")
+    else:
+        print("leap year")
+else:
+    print("not a leap year")
